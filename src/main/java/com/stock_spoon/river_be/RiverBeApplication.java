b11bd389a7622181e6a4673a4537714a8fea6451
syc71 <syc7179@naver.com>
2026-09-16 16:41:45 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/stock_spoon/river_be/RiverBeApplication.java
@@ -0,0 +1,13 @@
+package com.stock_spoon.river_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiverBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiverBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock_spoon/river_be/RiverBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_spoon.river_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
